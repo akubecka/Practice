1cbcaccef1c31054063b02f3e9b2b779491a4fba
Alex Kubecka <akubecka@stevens.edu>
2020-12-20 15:39:08 -0500

    added Fizzbuzz

---
+++ Java/Algos/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,17 @@
+/* Write a short program that prints each number from 1 to 100 on a new line. 
+
+For each multiple of 3, print "Fizz" instead of the number. 
+
+For each multiple of 5, print "Buzz" instead of the number. 
+
+For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+Write a solution (or reduce an existing one) so it has as few characters as possible. */
+
+
+public class FizzBuzz{
+    public static void main(String[] args){
+        String input = System.console().readLine();
+        System.out.println(input);
+    }
+}
+

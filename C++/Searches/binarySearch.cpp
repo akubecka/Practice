54a3337e117699374793337d7c473c59f9bdba9b
Alex Kubecka <akubecka@stevens.edu>
2020-12-20 16:01:53 -0500

    bin seach cpp

---
+++ C++/Searches/binarySearch.cpp
@@ -0,0 +1,74 @@
+// C++ program to implement recursive Binary Search 
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+// A recursive binary search function. It returns 
+// location of x in given array arr[l..r] is present, 
+// otherwise -1 
+int binarySearch(int arr[], int l, int r, int x) 
+{ 
+    if (r >= l) { 
+        int mid = l + (r - l) / 2; 
+  
+        // If the element is present at the middle 
+        // itself 
+        if (arr[mid] == x) 
+            return mid; 
+  
+        // If element is smaller than mid, then 
+        // it can only be present in left subarray 
+        if (arr[mid] > x) 
+            return binarySearch(arr, l, mid - 1, x); 
+  
+        // Else the element can only be present 
+        // in right subarray 
+        return binarySearch(arr, mid + 1, r, x); 
+    } 
+  
+    // We reach here when element is not 
+    // present in array 
+    return -1; 
+} 
+  
+
+
+// A iterative binary search function. It returns 
+// location of x in given array arr[l..r] if present, 
+// otherwise -1 
+int binarySearchI(int arr[], int l, int r, int x) 
+{ 
+    while (l <= r) { 
+        int m = l + (r - l) / 2; 
+  
+        // Check if x is present at mid 
+        if (arr[m] == x) 
+            return m; 
+  
+        // If x greater, ignore left half 
+        if (arr[m] < x) 
+            l = m + 1; 
+  
+        // If x is smaller, ignore right half 
+        else
+            r = m - 1; 
+    } 
+  
+    // if we reach here, then element was 
+    // not present 
+    return -1; 
+} 
+
+
+int main(void) 
+{ 
+    int arr[] = { 2, 3, 4, 10, 40 }; 
+    int x = 10; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    int resultR = binarySearch(arr, 0, n - 1, x); 
+    (resultR == -1) ? cout << "Recursive: Element is not present in array"
+                   : cout << "Recursive: Element is present at index " << resultR; 
+    int resultI = binarySearchI(arr, 0, n - 1, x); 
+    (resultI == -1) ? cout << "Iterative: Element is not present in array"
+                   : cout << "Iterative: Element is present at index " << resultI; 
+    return 0; 
+} 
